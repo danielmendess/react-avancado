b4b3d27965bfec4da05a29ba3b7bc9dd1809d6d6
Daniel Mendes <dmsdaniel11@gmail.com>
2023-02-01 00:39:10 -0300

    Usando Storybook Essentials e Controls

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -3,7 +3,11 @@ import { Meta, Story } from '@storybook/react/types-6-0'
 
 export default {
   title: 'Main',
-  component: Main
+  component: Main,
+  args: {
+    title: 'Default',
+    description: 'Description Default'
+  }
 } as Meta
 
-export const Basic: Story = () => <Main />
+export const Basic: Story = (args) => <Main {...args} />
